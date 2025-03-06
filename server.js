d973c2af571030a78cfcae6609a2b8d28214ca54
norton <mediasevenlab@gmail.com>
2025-03-06 11:57:03 +0700

    Fixed import issue for pg

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 import express from 'express';
 import cors from 'cors';
-import { Pool } from 'pg';
+import pkg from 'pg';
 import dotenv from 'dotenv';
 
 dotenv.config();
-const app = express();
+
+const { Pool } = pkg;
+
 const port = process.env.PORT || 5000;
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
 });
 
+const app = express();
 app.use(cors());
 app.use(express.json());
 
